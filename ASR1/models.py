ea214da23c604046bff0985ea37f8c34981b5b18
wareval0 <w.arevalo@uniandes.edu.co>
2024-03-16 08:36:34 -0500

    País form

--- ASR1/models.py
+++ ASR1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class DatosPersonales(models.Model):
     nombres = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=30)
-    #pais = models.CharField(max_length=15)
+    pais = models.CharField(max_length=15)
     ciudad = models.CharField(max_length=20)
     numero = models.CharField(max_length=30)
     email = models.EmailField()
