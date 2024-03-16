00b1474b11c7eded1539e0d38fa98f7598217687
wareval0 <w.arevalo@uniandes.edu.co>
2024-03-16 08:59:21 -0500

    Style Form

--- ASR1/forms.py
+++ ASR1/forms.py
@@ -5,4 +5,3 @@ class DatosPersonalesForm(forms.ModelForm):
     class Meta:
         model = DatosPersonales
         fields = ['nombres', 'apellidos', 'pais', 'ciudad', 'numero', 'email']
-        labels = {'nombres': 'Nombres', 'apellidos': 'Apellidos', 'pais': 'Pais', 'numero': 'Numero', 'email': 'E-Mail'}
